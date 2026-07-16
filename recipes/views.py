1dd30ddf48929ee00de9e206c95af7562a635726
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-07-16 15:33:38 -0300

    V 1.0 - Session 8 | fixing preparation-steps detail

--- recipes/views.py
+++ recipes/views.py
@@ -23,8 +23,15 @@ def category(request, category_id):
         'title': f'{recipes[0].category.name} - Category |',
     })
 
-    def recipe(request, id):
-        return render(request, 'recipes/pages/recipe-view.html', context={
-            'recipe': make_recipe(),
-            'is_detail_page': True,
-        })
+
+def recipe(request, id):
+
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
+    })
