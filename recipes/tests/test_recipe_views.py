c34973d0340ab4c065799502975788cfed05e8a0
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-07-28 07:47:05 -0300

    V 1.0 - Session 9 | Using self.client by Django to simulate the client in the tests

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -15,3 +15,12 @@ class RecipeViewsTest(TestCase):
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        # reverse é a função que nos  permite adicionar uma url dinamicamente
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
