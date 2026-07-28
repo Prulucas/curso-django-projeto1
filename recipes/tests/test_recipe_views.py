edd0fb2a3c6f07972caf5f2ee5f9bea301617691
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-07-28 07:57:56 -0300

    V 1.0 - Session 9 | Testing "No recipes found" in home.html

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -24,3 +24,8 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        # Função decode converte bytes em uma string
+        self.assertIn('No recipes found', response.content.decode('utf-8'))
