c1f76a2f19f502558a6cfd7f89e8c79cd9855cf9
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-07-31 22:56:29 -0300

    V 1.0 - Session 9 | testing the response context and content in the template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipeViewsTest(TestCase):
@@ -21,6 +22,36 @@ class RecipeViewsTest(TestCase):
         response = self.client.get(reverse('recipes:home'))
         # Função decode converte bytes em uma string
         self.assertIn('No recipes found', response.content.decode('utf-8'))
+
+    def test_recipe_home_template_loads_recipe(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unity='Minutos',  # <--- com unity (com y)
+            servings=5,
+            servings_unity='Porções',          # <--- com unity (com y)
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        response = self.client.get(reverse('recipes:home'))
+        response_context_recipes = response.context['recipes']
+
+        content = response.content.decode('utf-8')  # testar o template
+        self.assertIn('Recipe Title', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
