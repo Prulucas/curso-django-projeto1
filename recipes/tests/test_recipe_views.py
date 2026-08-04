72e4d1817a904662364ee8a597867c2581b34e82
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-08-04 08:07:25 -0300

    V 1.0 - Session 9 | Testing is_published in category and recipe detail

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -58,6 +58,16 @@ class RecipeViewsTest(RecipeTestBase):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
 
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_category_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
             resolve(reverse('recipes:category', kwargs={'category_id': 1}))
@@ -93,3 +103,14 @@ class RecipeViewsTest(RecipeTestBase):
 
         content = response.content.decode('utf-8')  # testar o template
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:recipe', kwargs={
+            'id': recipe.category.id,
+        }))
+
+        self.assertEqual(response.status_code, 404)
