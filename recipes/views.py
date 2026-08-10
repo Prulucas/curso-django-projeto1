7c79fe7f49bbaebaf193b94d587e5bcfe64b924f
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-08-10 18:30:24 -0300

    V 1.0 - Session 10 | Field filter contains and contains and complex queries with Q

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from recipes.models import Recipe
+from django.db.models import Q
 
 # from utils.recipes.factory import make_recipe
 
@@ -54,6 +55,15 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        # para buscar o que contem o search_term
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+        # o 'i' antes do contains é para ignorar variações de escrita
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes
     })
