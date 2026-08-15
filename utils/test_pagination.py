2eb491aa6a722ebaa6a983d668d789e6db73eeaf
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-08-15 16:30:14 -0300

    V 1.0 - Session 10 | Modifying the page range only when necessary

---
+++ utils/test_pagination.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+from utils.pagination import make_pagination_range
+
+
+class PaginationTest(TestCase):
+    def test_make_pagination_range_returns_a_pagination_range(self):  # noqa: E501
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_first_range_is_static_if_current_page_is_less_then_middle_page(self):  # noqa: E501
+        # Current page = 1 - Qty Page = 2 - Middle Page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # Current page = 2 - Qty Page = 2 - Middle Page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=2,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # Current page = 3 - Qty Page = 2 - Middle Page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=3,
+        )
+        self.assertEqual([2, 3, 4, 5], pagination)
+
+
+""" # noqa: E501
+Test no teminal
+python -m pytest utils/test_pagination.py -v
+"""
