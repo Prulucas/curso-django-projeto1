6881734e2387816ae64149914b9bae39a79b9310
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-08-21 22:49:36 -0300

    V 1.0 - Session 10 | Testing homepage pagination

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -1,7 +1,10 @@
 from django.urls import reverse, resolve
 from recipes import views
+from unittest.mock import patch
 # from unittest import skip
 from .test_recipe_base import RecipeTestBase
+
+from recipes.models import Recipe
 
 
 class RecipeHomeViewTest(RecipeTestBase):
@@ -53,3 +56,21 @@ class RecipeHomeViewTest(RecipeTestBase):
             expected_return,
             response.content.decode('utf-8')
         )
+
+    # @patch('recipes.views.PER_PAGE', new=3)
+    # |_> Utilizando patch como decorator
+    def test_recipe_home_is_paginated(self):
+
+        for i in range(9):
+            kwargs = {'slug': f'r{i}', 'author_data':  {'username': f'u{i}'}}
+            self.make_recipe(**kwargs)
+
+        with patch('recipes.views.PER_PAGE', new=3):
+            response = self.client.get(reverse('recipes:home'))
+            recipes = response.context['recipes']
+            paginator = recipes.paginator
+
+            self.assertEqual(paginator.num_pages, 3)
+            self.assertEqual(len(paginator.get_page(1)), 3)
+            self.assertEqual(len(paginator.get_page(2)), 3)
+            self.assertEqual(len(paginator.get_page(3)), 3)
