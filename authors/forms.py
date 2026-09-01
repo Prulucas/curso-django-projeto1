1eb48401010a798aaedc07a314ca6a8c761a77a9
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-09-01 11:58:28 -0300

    V 1.0 - Session 13 | Setting labels, widgets, errors, between others

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,28 @@ class RegisterForm(forms.ModelForm):
         # Class to pass META DATA To Forms.
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
+        # mostra todos os campos, excluindo o que eu escolhi
+        # exclude = ['first_name']
+        labels = {
+            'username': 'Username',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid'
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must be not be empty',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your username here'
+            }),  # referente ao que sera renderizado a tela
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
