17531bfff8dc0307366f7b3b47ec55dcd1e7229f
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-09-03 21:45:39 -0300

    V 1.0 - Session 10 | Using Clean_field to validate a field

--- authors/forms.py
+++ authors/forms.py
@@ -5,10 +5,16 @@ from django import forms
 # from django.forms import ModelForm
 
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
 
 
 def add_placeholder(field, placeholder_val):
-    field.widget.attrs['placeholder'] = placeholder_val
+    add_attr(field, 'placeholder', placeholder_val)
 
 
 class RegisterForm(forms.ModelForm):
@@ -70,3 +76,25 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here'
             })
         }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não digite %(value)s no campo password', code='invalid',
+                params={'value': '"atenção"'}
+            )
+
+        return data
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get('password')
+
+        if 'John Doe' in data:
+            raise ValidationError(
+                'Não digite %(value)s no campo first name', code='invalid',
+                params={'value': '"John Doe"'}
+            )
+
+        return data
