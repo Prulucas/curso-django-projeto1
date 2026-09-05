3f714b7782ef828c902959249c0f456b2b05f92c
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-09-05 16:51:22 -0300

    V 1.0 - Session 13 | is_valid and save data from the form to database

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http import Http404
+from django.contrib import messages
 
 from .forms import RegisterForm
 
@@ -21,5 +22,11 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST  # Salvando na sessão
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del (request.session['register_form_data'])
 
     return redirect('authors:register')
