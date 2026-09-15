163d8368192e17df710baa7022d34912dc189f6c
Pedro Lucas Silva <plbrandsilva@gmail.com>
2026-09-15 07:31:32 -0300

    V 1.0 - Session 13 | Creating URLs, views and template to log in

--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import redirect, render
-from django.http import Http404
 from django.contrib import messages
+from django.http import Http404
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from .forms import RegisterForm
 
@@ -12,6 +13,7 @@ def register_view(request):
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+        'form:action': reverse('authors:register_create'),
     })
 
 
@@ -32,3 +34,11 @@ def register_create(request):
         del (request.session['register_form_data'])
 
     return redirect('authors:register')
+
+
+def login_view(request):
+    return render(request, 'authors/pages/login.html')
+
+
+def login_create(request):
+    return render(request, 'authors/pages/login.html')
